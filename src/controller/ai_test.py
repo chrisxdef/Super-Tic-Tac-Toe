930c067a0201da62fa2d0f7eb9cf89efee78d624
Christopher Heisler <cheisler72@gmail.com>
2017-10-20 15:50:14 -0400

    Added winboard function

--- src/controller/ai_test.py
+++ src/controller/ai_test.py
@@ -6,6 +6,18 @@ n = Node(1,gameBoard)
 n.createTree()
 print 'Done createTree()'
 
-result = n.minimax(True)
-
-print result
+for child in n.children:
+	print child.gameBoard
+	for child2 in child.children:
+		print "\t"+str(child2.gameBoard)
+		for child3 in child2.children:
+			print "\t\t"+str(child3.gameBoard)
+			for child4 in child3.children:
+				print "\t\t\t"+str(child4.gameBoard)
+				for child5 in child4.children:
+					print "\t\t\t\t"+str(child5.gameBoard)
+					for child6 in child5.children:
+						print "\t\t\t\t\t"+str(child6.gameBoard)
+				
+		
+	
