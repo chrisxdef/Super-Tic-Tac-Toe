956709dc010ec2cdd1e1233fcb504a844fb68800
Christopher Heisler <cheisler72@gmail.com>
2017-11-03 14:38:17 -0400

    housekeeping

--- src/controller/ai.py
+++ src/controller/ai.py
@@ -1,4 +1,6 @@
 import sys
+sys.path.append("../view")
+from tic-tac-toe import winBoard
 from random import randint
 from sets import Set
 
@@ -48,31 +50,6 @@ class Node():
 		if mini:
 			return min(values)
 		return max(values)
-
-winBoards = [Set([0,1,2]),Set([3,4,5]),Set([6,7,8]),Set([0,3,6]),Set([1,4,7]),Set([2,5,8]),Set([0,4,8]),Set([2,4,6])]
-
-def fullBoard(board):
-	for i in board:
-		if i == 0:
-			return False
-	return True
-
-def winBoard(board):
-	player1 = []
-	player2 = []
-	for i in range(len(board)):
-		if board[i] == 1:
-			player1.append(i)
-		elif board[i] == 2:
-			player2.append(i)
-	player1 = Set(player1)
-	player2 = Set(player2)
-	for s in winBoards:
-		if s.issubset(player1):
-			return 1
-		elif s.issubset(player2):
-			return 2
-	return False
 
 	
 if __name__=="__main__":
