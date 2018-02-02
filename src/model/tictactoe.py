1e347261fa7d6da967585ed0d275429737827ced
antakij1 <antakij1@students.rowan.edu>
2018-02-02 05:09:32 -0500

    Working GUI

--- src/model/tictactoe.py
+++ src/model/tictactoe.py
@@ -1,70 +1,90 @@
 import sys
 from game_utils import winBoard, fullBoard
+
 sys.path.append("../controller")
 from ai import Node
 
-def game(gameBoard, human, ai):
-	running = True
-	turn = 1
-	while(running):
-		print gameBoard
-		if turn == human:
-			index = int(raw_input("Show me your moves!\t"))
-			while(gameBoard[index] != 0):
-				index = int(raw_input("Show me your moves!\t"))
-			gameBoard[index] = human
-			turn = ai
-		else:
-			print "Robot's Turn\n"
-			node = Node(ai, gameBoard)
-			node.createTree(3)
-			gameBoard = node.minimax(False, ai, True)
-			turn = human
-		if winBoard(gameBoard) == human:
-			print "GG! You Win!"
-			running = False
-		elif winBoard(gameBoard) == ai:
-			print "GG! Robot Wins!"
-			print gameBoard
-			running = False
-		elif fullBoard(gameBoard):
-			print "GG! It's a draw!"
-			running = False		
 
-superBoard = [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]
+def game(gameBoard, human, ai):
+    running = True
+    turn = 1
+    while (running):
+        print gameBoard
+        if turn == human:
+            index = int(raw_input("Show me your moves!\t"))
+            while (gameBoard[index] != 0):
+                index = int(raw_input("Show me your moves!\t"))
+            gameBoard[index] = human
+            turn = ai
+        else:
+            print "Robot's Turn\n"
+            node = Node(ai, gameBoard)
+            node.createTree(3)
+            gameBoard = node.minimax(False, ai, True)
+            turn = human
+        if winBoard(gameBoard) == human:
+            print "GG! You Win!"
+            running = False
+        elif winBoard(gameBoard) == ai:
+            print "GG! Robot Wins!"
+            print gameBoard
+            running = False
+        elif fullBoard(gameBoard):
+            print "GG! It's a draw!"
+            running = False
+
+
+superBoard = [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
 
 class GameState:
-	def __init__(self, gameBoard=superBoard, human=1):
-		self.gameBoard = gameBoard
-		self.human = human
-		self.ai = 2 if human == 1 else 1
-		self.prompts = { self.human : "Show me your moves!", self.ai : "Robot's move."}
-		self.turn = 1
-		self.prompt = "Welcome to Super Tic-Tac-Toe!\n" + self.prompts[self.turn]
-		self.gameOver = False
+    def __init__(self, gameBoard=superBoard, human=1):
+        self.gameBoard = gameBoard
+        self.human = human
+        self.ai = 2 if human == 1 else 1
+        self.prompts = {self.human: "Show me your moves!", self.ai: "Robot's move."}
+        self.turn = 1
+        self.prompt = "Welcome to Tic-Tac-Toe!\n" + self.prompts[self.turn]
+        self.gameOver = False
 
-	def update(self, move=None):
-		if move == None:
-			n = SuperNode(self.turn, self.gameBoard)
-			n.createTree(4)
-			move = ai.minimax(False, self.ai)
-		self.gameBoard = move
-		self.gameOver = self.checkEnd()
-		if(self.gameOver):
-			self.prompt = "Game Over"
-		else:			
-			self.turn = 1 if self.turn == 2 else 1
-			self.prompt = self.prompts[self.turn]
-	
-	def checkEnd(self):
-		if(fullBoard(self.gameBoard)):
-			return 3
-		else:
-			return winBoard(self.gameBoard)
+    def getGameOver(self):
+        return self.gameOver
+
+    def getPrompt(self):
+        return self.prompt
+
+    def getBoard(self):
+        return self.gameBoard
+
+    def updateAI(self):
+        n = Node(self.turn, self.gameBoard)
+        n.createTree(3)
+        self.gameBoard = n.minimax(False, self.ai, True)
+        self.changeTurn()
+
+    def updateHuman(self, index):
+        self.gameBoard[index] = self.human
+        self.changeTurn()
+
+    def changeTurn(self):
+        self.gameOver = self.checkEnd()
+        if (self.gameOver):
+            self.prompt = "Game Over"
+        else:
+            self.turn = 1 if self.turn == 2 else 2
+            self.prompt = self.prompts[self.turn]
+
+    def checkEnd(self):
+        if (fullBoard(self.gameBoard)):
+            return 3
+        else:
+            return winBoard(self.gameBoard)
 
 
 if __name__ == "__main__":
-	gameBoard = [0,0,0,0,0,0,0,0,0]
-	human = 1
-	ai = 2
-	game(gameBoard, human, ai)	
+    gameBoard = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    human = 1
+    ai = 2
+    game(gameBoard, human, ai)
